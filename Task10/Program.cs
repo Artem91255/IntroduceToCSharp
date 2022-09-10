f11cb9db555fab28ab4977905970e251611009a3
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:45:16 +0300

    Task10

---
+++ Task10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number/10)%10;
+
+Console.WriteLine("Вторая цифра числа это "+result);
