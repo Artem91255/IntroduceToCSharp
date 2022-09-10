8ea774b07cd3298f6cbe5eb099af82bcb2e22b8d
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:39:19 +0300

    Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 29. Напишите программу, которая задает массив из 8 элементов и выводит их на экран*/
+
+Console.Clear();
+
+int[] array = new int[8];
+for(int i=0; i<array.Length; i++)
+{
+    int number = new Random().Next(1,1000);
+    array[i] = number;
+    Console.Write(array[i] + ", ");
+    
+}
+//Второй вариант вывода массива на экран
+Console.WriteLine("[{0}]", string.Join(",", array));
