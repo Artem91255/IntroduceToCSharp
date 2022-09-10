f8410d459a36dfad668030d9c6d35911cc3940b6
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 09:40:20 +0300

    task48

---
+++ Task48/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+
+int[,] CreateArray(string text)
+{
+    Console.WriteLine(text);
+    Console.WriteLine("Введите количество строк");
+    int rows = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите количество столбцов");
+    int columns = Convert.ToInt32(Console.ReadLine());
+    int[,] array = new int[rows,columns];
+    return array;
+}
+
+void FillArray(int [,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            array[i,j]= i+j;
+        }
+    }
+}
+void Print(int[,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Array2D = CreateArray("Введите параметры создаваемого массива: ");
+FillArray(Array2D);
+Print(Array2D);
