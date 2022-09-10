d2807ff8b192c00593912f5cf74890b96fd9b8ab
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:26:01 +0300

    Task33

---
+++ Task33/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+
+int[] array2 = { 6, 7, 5, 9, 3, 1 };
+System.Console.WriteLine("Введите искомое число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (array2.Contains(x))
+{
+    System.Console.WriteLine("Да");
+}
+else
+{
+    System.Console.WriteLine("Нет");
+}
+
