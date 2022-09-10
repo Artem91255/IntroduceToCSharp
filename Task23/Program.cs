c8222a907623658c3568ac8acaba787d0281c650
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:43:10 +0300

    Task23

---
+++ Task23/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+sheet(x);
+
+void sheet(int number)
+{
+double[] array = new double[number];
+for(int i = 1; i<=number; i++)
+{
+   array[i-1] = Math.Pow(i,3);
+   Console.Write(array[i-1] + " ");
+}
+}
+
+
