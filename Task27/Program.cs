8e80d321eba4c757c6a05f08455c5c9cecc5b02e
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:38:20 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдает сумму цифр в числе*/
+
+Console.Clear();
+
+Console.WriteLine("Введите число");
+string num = Console.ReadLine();
+Console.WriteLine("Сумма цифр введенного числа равна " + SumOfNumbers(num));
+
+
+int SumOfNumbers(string number)
+{
+    //string to char
+    int sum=0;
+    char[] charArray = number.ToCharArray();
+
+    //char to int
+    int[] intArray = new int[charArray.Length];
+    for(int i =0; i<charArray.Length; i++)
+    {
+        intArray[i]=charArray[i]-'0';
+        sum=sum+intArray[i];
+  
+    }
+    return sum;
+}
