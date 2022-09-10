e3db1d582a01d3fa7486f6333cf8897040a64dbe
artem.chernomazov <pechenkazz@mail.ru>
2022-09-10 15:14:46 +0300

    Task38

---
+++ Task38/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Задача 38: Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+Console.Clear();
+
+
+int[] arrayOfNumbers = new int[8];
+for(int i=0; i<arrayOfNumbers.Length; i++)
+{
+    int number = new Random().Next();
+    arrayOfNumbers[i] = number;
+}
+Console.WriteLine("[{0}]", string.Join(",", arrayOfNumbers));
+Console.WriteLine(Diff(arrayOfNumbers));
+
+int Diff(int[] array)
+{
+    int max = array[0];
+    for(int i = 0; i<array.Length; i++)
+    {
+        if (array[i]>max)  max = array[i];
+    }
+    int min = array[0];
+    for(int i = 0; i<array.Length; i++)
+    {
+        if (array[i]<min)  min = array[i];
+    }
+
+    int difference = max-min;
+    return difference;
+}
