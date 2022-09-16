74faaa2da0d965c53859efac576bbc9bcf400a51
artem.chernomazov <pechenkazz@mail.ru>
2022-09-16 09:18:17 +0300

    вывод всех натуральных чисел в промежутке от M до N

---
+++ Task65/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+int NumbersToN(int number1, int number2)
+{
+    
+    if(number1>number2) return number2;
+    
+    else
+    {
+        Console.Write(number1+ ", ");
+        number1++;
+    
+    }
+    
+    return NumbersToN(number1++, number2);
+
+}
+
+Console.WriteLine("Enter numStart: ");
+int numStart = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter numFinish: ");
+int numFinish = Convert.ToInt32(Console.ReadLine());
+
+NumbersToN(numStart,numFinish);
